958c7d2e2cb67356871911cca5e6ff60491a94e5
Juan David Sanguino <juandavidsanguinoo@gmail.com>
2022-10-22 21:41:17 -0500

    feat: veterinary appoinments scheduling

--- src/App.js
+++ src/App.js
@@ -1,24 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import Form from "./components/Form";
+import Appointment from "./components/Appointment";
 
 function App() {
+
+  // Appointments in local storage
+  let initialAppointments = JSON.parse(localStorage.getItem("appointments"))
+  if (!initialAppointments) {
+    initialAppointments = [];
+  }
+
+  // Array of appointments
+  const [appointments, setAppointments] = useState(initialAppointments);
+
+  //Use effect to perform certain actions when the state changes
+  useEffect(() => {
+    let initialAppointments = JSON.parse(localStorage.getItem("appointments"));
+    if (initialAppointments) {
+      localStorage.setItem("appointments", JSON.stringify(appointments));
+    } else {
+      localStorage.setItem("appointments", JSON.stringify([]));
+    }
+  }
+    , [appointments]);
+
+  // Function to take current appointments and add the new one
+  const createAppointment = (appointment) => {
+    setAppointments([...appointments, appointment]);
+  };
+
+  // Function to delete an appointment by its ID
+  const deleteAppointment = (id) => {
+    const newAppointments = appointments.filter(
+      (appointment) => appointment.id !== id
+    );
+    setAppointments(newAppointments);
+  };
+
+  // Conditional message
+  const title =
+    appointments.length === 0 ? "No Appointments" : "Manage your appointments";
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Appointment Scheduling</h1>
+
+      <div className="container">
+        <div className="row">
+          <div className="one-half column">
+            <Form createAppointment={createAppointment} />
+          </div>
+          <div className="one-half column">
+            <h2>{title}</h2>
+            {appointments.map((appointment) => (
+              <Appointment
+                key={appointment.id}
+                appointment={appointment}
+                deleteAppointment={deleteAppointment}
+              />
+            ))}
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
---
+++ src/components/Form.js
@@ -0,0 +1,119 @@
+import React from 'react'
+import { useState } from 'react';
+import { v4 as uuidv4 } from "uuid";
+import PropTypes from 'prop-types';
+
+const Form = ({ createAppointment }) => {
+  const [appointment, setAppointment] = useState({
+    pet: "",
+    owner: "",
+    date: "",
+    time: "",
+    symptoms: "",
+  });
+
+  const [error, setError] = useState(false);
+
+  const handleChange = (e) => {
+    setAppointment({
+      ...appointment,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const { pet, owner, date, time, symptoms } = appointment;
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Validate
+    if ( pet.trim() === "" || owner.trim() === "" || date.trim() === "" || time.trim() === "" || symptoms.trim() === "") {
+      setError(true);
+      return;
+    }
+
+    // Delete previous message
+    setError(false);
+
+    // Assign ID
+    appointment.id = uuidv4();
+
+    // Create appointment
+    createAppointment(appointment);
+
+    // Reset form
+    setAppointment({
+      pet: "",
+      owner: "",
+      date: "",
+      time: "",
+      symptoms: "",
+    });
+  };
+
+  return (
+    <>
+      <h2>Create Appointment</h2>
+
+      {error ? <p className="alert-error">All fields are required</p> : null}
+
+      <form onSubmit={handleSubmit}>
+        <label>Pet's Name</label>
+        <input
+          type="text"
+          name="pet"
+          className="u-full-width"
+          placeholder="Pet's Name"
+          onChange={handleChange}
+          value={pet}
+        />
+
+        <label>Owner´s Name</label>
+        <input
+          type="text"
+          name="owner"
+          className="u-full-width"
+          placeholder="Owner´s Name"
+          onChange={handleChange}
+          value={owner}
+        />
+
+        <label>Date</label>
+        <input
+          type="date"
+          name="date"
+          className="u-full-width"
+          onChange={handleChange}
+          value={date}
+        />
+
+        <label>Time</label>
+        <input
+          type="time"
+          name="time"
+          className="u-full-width"
+          onChange={handleChange}
+          value={time}
+        />
+
+        <label>Symptoms</label>
+        <textarea
+          className="u-full-width"
+          name="symptoms"
+          onChange={handleChange}
+          value={symptoms}
+        ></textarea>
+
+        <button type="submit" className="u-full-width button-primary">
+          Add Appointment
+        </button>
+      </form>
+    </>
+  );
+};
+
+Form.propTypes = {
+  createAppointment: PropTypes.func.isRequired,
+}
+
+export default Form
